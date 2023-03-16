464793eb1efd2622569b4718e2cd34c824941eb6
JaIoI <mirzamurodov.jalol@gmail.com>
2023-03-16 13:55:51 +0500

    settings

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,17 @@
 import './vendor';
 import './helpers';
 import './components/social';
-import {ieFix} from './vendor/ie-fix';
-import {vhFix} from './vendor/vh-fix';
-import {actualYear} from './modules/actualYear';
+import { ieFix } from './vendor/ie-fix';
+import { vhFix } from './vendor/vh-fix';
+import { actualYear } from './modules/actualYear';
 import header from './components/header';
 import lazyLoading from './modules/lazyLoading';
 import scrollToAnchor from './modules/scrollToAnchor';
+
+
+import './components/jquery.nice-select';
+
+$('select').niceSelect();
 
 ieFix();
 vhFix();
